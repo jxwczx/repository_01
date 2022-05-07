a0c12bd120316c18002f8515a9fe8e2e8d730b6d
JXWCZX <2424749442@qq.com>
2022-05-07 21:31:40 +0800

    新建 test branch java class

--- jc/src/test/java/jc/jdbctemplate/TestBranch.java
+++ jc/src/test/java/jc/jdbctemplate/TestBranch.java
@@ -2,6 +2,8 @@ package jc.jdbctemplate;
 
 public class TestBranch {
     public void test(){
-        System.out.println("11111111");
+        System.out.println("11111SSSS1111");
+        System.out.println("11111DDDD1111");
+        System.out.println("11111GGGGG111");
     };
 }
